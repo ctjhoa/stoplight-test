535f72292f5f104b76e551769193786aa8db11a2
Camille TJHOA <ctjhoa@qovery.com>
2024-04-11 17:47:56 +0200

    Added 2 files

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjI0Mjk1Mw/spectral.js?branch_slug=master&token=309ed870-e4fd-4424-8b93-25f473718b69";
+export default { extends: ruleset };
